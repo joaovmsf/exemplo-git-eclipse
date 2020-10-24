258534bfb92e04b717f9459339abd150b1df399e
João Vitor <Joo Vitor@DESKTOP-B01IVPU>
2020-10-24 15:00:25 -0300

    upload de teste

---
+++ exemplo-git-eclipse/src/msg/MsgGit.java
@@ -0,0 +1,13 @@
+package msg;
+
+import javax.swing.JOptionPane;
+
+public class MsgGit {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Olá, Git!");
+
+	}
+
+}
